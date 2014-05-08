6213e0db3fc72037dcb43b7add0439007b800106
Brian van Burken <info@brianvanburken.nl>
2014-05-08 13:30:45 +0200

    Changed order of execution on after_create calls.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -21,9 +21,9 @@ class Notice
   index(:err_id => 1, :created_at => 1, :_id => 1)
 
   after_create :cache_attributes_on_problem, :unresolve_problem
+  after_create :set_urgence_on_problem
   after_create :email_notification
   after_create :services_notification
-  after_create :set_urgence_on_problem
   before_save :sanitize
   before_destroy :decrease_counter_cache, :remove_cached_attributes_from_problem
 
